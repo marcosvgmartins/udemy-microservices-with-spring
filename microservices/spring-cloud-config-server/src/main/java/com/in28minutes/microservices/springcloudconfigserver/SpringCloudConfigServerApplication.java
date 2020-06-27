6088c1e0b2e57932cb4af96e2534aed647116698
Marcos Martins <marcosvgmartins@gmail.com>
2020-06-27 13:18:36 -0300

    Finish the Spring Cloud Config Server module

--- microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,19 @@ package com.in28minutes.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * The EnableConfigServer will allow the app to return configuration for
+ * different microservices and environments.
+ *
+ * For example, to retrieve the QA environment config for limits-service:
+ * GET http://localhost:8888/limits-service/qa
+ *
+ * The property files must be commited to a local Git repository for them
+ * to be picked up (the repository URI is defined in this project's application.properties)
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
