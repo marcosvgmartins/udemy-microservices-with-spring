b839561432d715aead21f20a6a64dce98db82696
Marcos Martins <marcosvgmartins@gmail.com>
2020-06-28 18:54:57 -0300

    Route requests through the Zuul API Gateway

--- microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -12,12 +12,21 @@ import org.springframework.web.bind.annotation.PathVariable;
  * of another service. So, when we add Ribbon, we can stop hard-coding the url of the
  * service we want to communicate with (first it comes from the properties file, then from
  * the name server)
+ *
+ * After we added the Zuul API Gateway, we changed the endpoint at the FeignClient annotation
+ * so that the request would be sent through the API gateway
  */
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    /**
+     * The GetMapping path also changed after after adding Zuul, since we now have to prefix
+     * it with the application name (the same name that the exchange service registers in Eureka)
+     */
+    //@GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
