1174047a5f2e1e8727f777191b18ab9e4fff460c
Marcos Martins <marcosvgmartins@gmail.com>
2020-07-04 13:00:48 -0300

    Add distributed tracing with Zipkin and RabbitMQ

--- microservices/netflix-eureka-naming-server/src/main/java/com/in28minutes/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ microservices/netflix-eureka-naming-server/src/main/java/com/in28minutes/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * You can access http://localhost:8761/ to see the Eurke UI, with information about
+ * all the registered services
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
