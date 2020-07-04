8dc1e9535a3979206d8f2ac03d7d1722c2f35baa
Marcos Martins <marcosvgmartins@gmail.com>
2020-07-04 16:19:55 -0300

    Add Spring Cloud Bus and Hystrix

--- microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,26 @@ public class LimitsConfigurationController {
     @Autowired
     private Configuration configuration;
 
+    private static final int DEFAULT_MIN = 12;
+
+    private static final int DEFAULT_MAX = 28;
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration() {
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    /**
+     * The idea of this method is to demonstrate how fault tolerance with Hystrix can be enabled
+     * We can define a fallback method that gets executed if the target method throws an exception
+     */
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackForRetrieveWithFaultTolerance")
+    public LimitConfiguration retrieveWithFaultTolerance() {
+        throw new RuntimeException("Hystrix, please come to the rescue!");
+    }
+
+    public LimitConfiguration fallbackForRetrieveWithFaultTolerance() {
+        return new LimitConfiguration(DEFAULT_MAX, DEFAULT_MIN);
     }
 }
